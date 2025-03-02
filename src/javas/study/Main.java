f7daceab1f79959a16b8c1890629a92f04032035
twocowsong <twocowsong@naver.com>
2025-03-02 14:22:19 +0900

    알고리즘 - 2

---
+++ src/javas/study/Main.java
@@ -0,0 +1,18 @@
+package javas.study;
+
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] solution(int []arr) {
+        Stack<Integer> stack = new Stack<>();
+        Integer[] array = stack.toArray(new Integer[0]);
+
+
+    }
+
+}
